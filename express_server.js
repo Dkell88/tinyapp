e528b578840b177cd3e6f3d281afdf76a6826890
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-20 09:39:20 -0700

    User feature complete

--- express_server.js
+++ express_server.js
@@ -10,18 +10,43 @@ app.use(morgan('dev'));
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-const gererateRandomString = function() {
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+const gererateRandomString = function(length, alphaNum) {
   let result = '';
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 6; i > 0; --i) {
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  if (!alphaNum) {
+    chars = chars.slice(52);
+  }
+  for (let i = length; i > 0; --i) {
     result += chars[Math.floor(Math.random() * chars.length)];
   }
   return result;
 };
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+const checkEmails = function(email) {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return user;
+    }
+  }
+  return false;
 };
 
 app.get("/", (req, res) => {
@@ -30,14 +55,14 @@ app.get("/", (req, res) => {
 
 app.get("/urls/new", (req,res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    "user_id": req.cookies["user_id"]
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    "user_id": req.cookies["user_id"],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
@@ -45,7 +70,7 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/:shortURl", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    "user_id": req.cookies["user_id"],
     shortURL: req.params.shortURl,
     longURL: urlDatabase[req.params.shortURl]
   };
@@ -58,6 +83,25 @@ app.get("/u/:shortURl", (req ,res) => {
   const longURL = urlDatabase[req.params.shortURl];
   res.redirect(longURL);
 });
+
+
+
+app.get("/register", (req, res) => {
+  const templateVars = {
+    "user_id": req.cookies["user_id"],
+  };
+  res.render("urls_register", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  const templateVars = {
+    "user_id": req.cookies["user_id"],
+  };
+  res.render("urls_login", templateVars);
+});
+
+
+
 
 app.post("/urls/:shortURl/delete", (req, res) => {
   delete urlDatabase[req.params.shortURl];
@@ -77,21 +121,51 @@ app.post("/urls/:shortURl/edit", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  const randomChars = gererateRandomString();
+  const randomChars = gererateRandomString(6, true);
   urlDatabase[randomChars] = req.body.longURL;
   console.log("The short Url created is: ", randomChars);
   res.redirect(`/urls/${randomChars}`);
 });
   
 app.post("/login", (req, res) => {
-  console.log("The short Url created is: ", req.body.username);
-  res.cookie('username', req.body.username);
-  res.redirect(`/urls`);
+  console.log("The login post");
+  console.log("The login email", req.body.email);
+  console.log("The login password", req.body.password);
+  const user = checkEmails(req.body.email);
+  if (user === false) {
+    res.send("403 error, Email not found");
+  } else if (users[user].password !== req.body.password) {
+    res.send("403 error, password incorrect");
+  } else if (users[user].password === req.body.password) {
+    res.cookie('user_id', users[user].email);
+    res.redirect(`/urls`);
+  }
 });
 
 app.post("/logout", (req, res) => {
   console.log("Logout button pressed");
-  res.clearCookie('username');
+  res.clearCookie('user_id');
+  res.redirect(`/urls`);
+});
+
+app.post("/register", (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.send("400 error, missing field");
+    return;
+  }
+  console.log(checkEmails(req.body.email));
+  if (checkEmails(req.body.email) !== false) {
+    res.send("400 error, email already exists");
+    return;
+  }
+  const randomChars = gererateRandomString(4, false);
+  users[randomChars] = {
+    id: randomChars,
+    email: req.body.email,
+    password: req.body.password
+  };
+  console.log(users);
+  res.cookie('user_id', users[randomChars].email);
   res.redirect(`/urls`);
 });
 
@@ -100,7 +174,3 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-
-// app.get("/hello", (req,res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
